ae16dfdd980898e23e8a170e96614f6b3546f13e
Max Geuer <geuermax@gmail.com>
2020-11-04 11:44:38 +0100

    added power to calculation

--- OhmschegesetzRechner/MainWindow.xaml.cs
+++ OhmschegesetzRechner/MainWindow.xaml.cs
@@ -24,8 +24,6 @@ namespace OhmschegesetzRechner
 
         }
 
-
-      
         /// <summary>
         /// Eingabefelder überprüfen und ggf. Berechnung starten. Wird bei jeder Änderung in einem Eingabefeld ausgeführt.
         /// </summary>
@@ -34,116 +32,98 @@ namespace OhmschegesetzRechner
         private void CheckInputs(object sender, TextChangedEventArgs e)
         {
             // Eingaben entnehmen
-            string textCurrent = this.txtCurrent.Text;
             string textVoltage = this.txtVoltage.Text;
+            string textCurrent = this.txtCurrent.Text;
             string textResistence = this.txtResistence.Text;
+            string textPower = this.txtPower.Text;
 
             // Zürücksetzen der Ergebnisse
             this.ClearOutputs();
 
-            // Überprüfen ob ein Eingabefeld noch frei ist
-            bool isOneInputLeft = textCurrent.Equals(String.Empty) || textVoltage.Equals(String.Empty) || textResistence.Equals(String.Empty);            
+            bool[] isInputSet = new bool[4];
 
-            // show / hide Errormessage
-            if (!isOneInputLeft)
+            isInputSet[0] = !textVoltage.Equals(String.Empty);
+            isInputSet[1] = !textCurrent.Equals(String.Empty);
+            isInputSet[2] = !textResistence.Equals(String.Empty);
+            isInputSet[3] = !textPower.Equals(String.Empty);
+
+            int inputsSet = 0;
+            foreach(bool isSet in isInputSet)
             {
-                this.ErrorLabel.Content = "Ein Feld muss leer bleiben.";
-                this.ErrorLabel.Visibility = Visibility.Visible;
-            } else
-            {
-                // Fehlermeldung ausblenden
-                this.ErrorLabel.Visibility = Visibility.Hidden;
-
-                // Ermittlen welcher Wert berechnet werden muss.
-                bool voltageIsMissing = textVoltage.Equals(String.Empty) && !textResistence.Equals(String.Empty) && !textCurrent.Equals(String.Empty);
-                bool currentIsMussing = !textVoltage.Equals(String.Empty) && !textResistence.Equals(String.Empty) && textCurrent.Equals(String.Empty);
-                bool resistenceIsMissing = !textVoltage.Equals(String.Empty) && textResistence.Equals(String.Empty) && !textCurrent.Equals(String.Empty);
-
-                if (voltageIsMissing || currentIsMussing || resistenceIsMissing)
+                if (isSet)
                 {
-                    double voltage = 0;
-                    double resistence = 0;
-                    double current = 0;
-
-                    try
-                    {
-                        // Die Eingabe in eine Kommazahl konvertieren
-                        if (!voltageIsMissing)
-                        {
-                            voltage = Convert.ToDouble(textVoltage);
-                        }
-                        if (!currentIsMussing)
-                        {
-                            current = Convert.ToDouble(textCurrent);
-                        }
-                        if (!resistenceIsMissing)
-                        {
-                            resistence = Convert.ToDouble(textResistence);
-                        }
-                        // call Function
-                        CalculateMissingValue(voltage, resistence, current);
-                    }
-                    // Abfangen ener ungültigen Zahl.
-                    catch (FormatException ex)
-                    {
-                        // Fehler angeben, wenn die Konvertierung fehlgeschlagen ist.
-                        this.ErrorLabel.Content = "Ein eingegebener Wert ist ungültig.";
-                        this.ErrorLabel.Visibility = Visibility.Visible;
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-
-                // Fehler anzeigen, wenn nicht mindestens zwei Felder ausgefüllt sind.
-                if (!voltageIsMissing && !currentIsMussing && !resistenceIsMissing)
-                {
-                    this.ErrorLabel.Content = "Es müssen mindestens zwei Werte gegeben sein.";
-                    this.ErrorLabel.Visibility = Visibility.Visible;
+                    inputsSet += 1;
                 }
             }
-            
-        }
 
+            // Fehlermeldung ausblenden
+            this.ErrorLabel.Visibility = Visibility.Hidden;
 
-        /// <summary>
-        /// Berechnen des Fehlenden Wertes und diesen in der Oberfläche anzeigen.
-        /// </summary>
-        /// <param name="voltage">Spannung - Wenn fehlend ist diese 0.</param>
-        /// <param name="resistence">Widerstand - Wenn fehlend ist dieser 0.</param>
-        /// <param name="current">Strom - Wenn fehlend ist dieser 0.</param>
-        public void CalculateMissingValue(double voltage = 0, double resistence = 0, double current = 0 )
-        {
-            // Zahl mit -1 multiplizieren wenn dieser > 0 ist
-            voltage = voltage < 0 ? voltage * -1 : voltage;
-            resistence = resistence < 0 ? resistence * -1 : resistence;
-            current = current < 0 ? current * -1 : current;
-            
-
-
-            if (voltage == 0)
+            if (inputsSet <= 0)
             {
-                // berechne Spannung
-                double value = resistence * current;
-                value = Math.Round(value, 3);
-                this.outVoltage.Content = value.ToString() + " V";
-                this.outCurrent.Content = current.ToString() + " A";
-                this.outResistence.Content = resistence.ToString() + " \u2126";
+                // kein Wert ist eingegeben
+            } else if (inputsSet == 1)
+            {
+                // Fehler es müssen 2 gesetzt sein
+                this.ErrorLabel.Content = "Es müssen 2 Felder ausgefüllt sein.";
+                this.ErrorLabel.Visibility = Visibility.Visible;
+            } else if (inputsSet == 2)
+            {
+                // Berechnen
+                // Convert string to double
+                double voltage = !textVoltage.Equals(String.Empty) ? Convert.ToDouble(textVoltage) : 0;
+                double current = !textCurrent.Equals(String.Empty) ? Convert.ToDouble(textCurrent) : 0;
+                double resistence = !textResistence.Equals(String.Empty) ? Convert.ToDouble(textResistence) : 0;
+                double power = !textPower.Equals(String.Empty) ? Convert.ToDouble(textPower) : 0;
 
-            } else if (resistence == 0)
+                if (voltage == 0)
+                {
+                    // Try to calc voltage
+                    if (current != 0 && resistence != 0)
+                    {
+                        voltage = CalcVoltage(current, resistence);
+                    } else if (current != 0 && power != 0) 
+                    {
+                        voltage = CalcVoltageByPower(power, current);
+                    }
+                }
+
+                if (current == 0)
+                {
+                    if (voltage != 0 && resistence != 0)
+                    {
+                        current = CalcCurrent(voltage, resistence);
+                    } else if (power != 0 &&  resistence != 0) 
+                    {
+                        current = CalcCurrentByPowerAndResistence(power, resistence);
+                    } else if (power != 0 && voltage != 0)
+                    {
+                        current = CalcCurrentByPowerAndVoltage(power, voltage);
+                    }
+                }
+
+                if (resistence == 0)
+                {
+                    if (voltage != 0 && current != 0)
+                    {
+                        resistence = CalcResistence(voltage, current);
+                    } else if (power != 0 && current != 0)
+                    {
+                        resistence = CalcResistenceByPower(power, current);
+                    }
+                }
+
+                if (power == 0)
+                {
+                    power = CalcPower(voltage, current);
+                }
+
+                SetOutput(voltage, resistence, current, power);
+            } else 
             {
-                // berechne Widerstand
-                double value = voltage / current;
-                value = Math.Round(value, 3);
-                this.outResistence.Content = value.ToString() + " \u2126";
-                this.outVoltage.Content = voltage.ToString() + " V";
-                this.outCurrent.Content = current.ToString() + " A";
-            } else
-            {
-                // berechne Strom
-                double value = voltage / resistence;
-                value = Math.Round(value, 3);
-                this.outCurrent.Content = value.ToString() + " A";
-                this.outVoltage.Content = voltage.ToString() + " V";
-                this.outResistence.Content = resistence.ToString() + " \u2126";
+                // Fehler es dürfen nur 2 gesetzt sein
+                this.ErrorLabel.Content = "Es dürfen nur 2 Felder ausgefüllt sein.";
+                this.ErrorLabel.Visibility = Visibility.Visible;
             }
         }
 
@@ -155,6 +135,56 @@ namespace OhmschegesetzRechner
             this.outCurrent.Content = "";
             this.outResistence.Content = "";
             this.outVoltage.Content = "";
+            this.outPower.Content = "";
+        }
+
+        private double CalcPower(double current, double voltage)
+        {
+            return current * voltage;
+        }
+
+        private double CalcCurrent(double voltage, double resistence) 
+        {
+            return voltage / resistence;
+        }
+
+        private double CalcCurrentByPowerAndVoltage(double power, double voltage)
+        {
+            return power / voltage;
+        }
+
+        private double CalcCurrentByPowerAndResistence(double power, double resistence)
+        {
+            return Math.Sqrt(power / resistence);
+        }
+
+        private double CalcVoltage(double current, double resistence)
+        {
+            return current * resistence;
+        }
+
+        private double CalcVoltageByPower(double power, double current)
+        {
+            return power / current;
+        }        
+
+        private double CalcResistence(double voltage, double current)
+        {
+            return voltage / current;
+        }
+
+        private double CalcResistenceByPower(double power, double current)
+        {
+            return power / Math.Pow(current, 2);
+        }
+
+
+        private void SetOutput(double voltage, double resistence, double current, double power)
+        {
+            this.outCurrent.Content = Math.Round(current, 3).ToString() + " A";
+            this.outVoltage.Content = Math.Round(voltage, 3).ToString() + " V";
+            this.outResistence.Content = Math.Round(resistence, 3).ToString() + " \u2126";
+            this.outPower.Content = Math.Round(power, 3).ToString() + " W";
         }
         
         /// <summary>
@@ -204,8 +234,22 @@ namespace OhmschegesetzRechner
         private void btnHelpCurrent_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(
-                "Der elektrische Strom ist die Anzahl an elektischen Ladungsträgern, die innerhalb eines Stromkreises fließen \n\nFormelzeichen : I\nEinheit : Ampere / A",
+                "Der elektrische Strom ist die Anzahl an elektischen Ladungsträgern, die innerhalb eines Stromkreises fließen. \n\nFormelzeichen : I\nEinheit : Ampere / A",
                 "Hilfe zum Strom",
+                MessageBoxButton.OK
+                );
+        }
+
+        /// <summary>
+        /// Hilfe zur elektischen Leistung anzeigen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnHelpPower_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Die elektrische Leistung gibt die in eriner Zeitspanne umgesetzte Energie an. \n\nFormelzeichen : P\nEinheit : Watt / W",
+                "Hilfe zur Leistung",
                 MessageBoxButton.OK
                 );
         }
